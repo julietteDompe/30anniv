5880fa97502cf1da9ccea0e76f71730a44ed137f
julietteDompe <d.juliette02@gmail.com>
2023-01-27 13:31:09 +0100

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Juju Bday',
       theme: ThemeData(
         primarySwatch: MaterialColor(0xFFF9F3E7, <int, Color>{
           50: JujuColors.gradientBottom,
